c5cd4e13c5b31dd647113e03ad1ed480f858c6bf
FVL-CREATOR <francesc.vivo@campus.xaloc.org>
2024-10-01 10:08:11 +0200

    Add files via upload

---
+++ Ejercicio 9.py
@@ -0,0 +1,7 @@
+#9.. programa que pida los segundos y muestre por pantalla y en la misma frase los minutos 
+#y las horas
+
+var1=float(input("¿qué numero de segundos quieres?: "))
+minutos=var1/60
+horas=minutos/60
+print("el numero de minutos es: ",minutos,"y en horas es:",horas)
