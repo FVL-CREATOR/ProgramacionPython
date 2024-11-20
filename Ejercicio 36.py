b2df98ae29832f6840b021c449e3ad4aef62d99f
FVL-CREATOR <francesc.vivo@campus.xaloc.org>
2024-11-20 13:30:12 +0100

    Add files via upload

---
+++ Ejercicio 36.py
@@ -0,0 +1,9 @@
+#36. Programa que sume los n primeros números naturales. n Lo introduce el usuario.
+
+n=int(input("Introduce el numero de los primeros numeros naturales que quieres se sumen: "))
+var1=0
+for contador in range (0,n+1):
+    var1=var1+contador
+print("La suma de numeros es",var1)
+
+         
